04d108aabc973aa4f004a1ffab841c10249ee595
Henry Tao <henry.tao@shopify.com>
2019-02-08 13:28:34 -0500

    Resolve comments in PR 21

--- livedata-ktx/src/main/java/com/shopify/livedataktx/SupportMediatorLiveData.kt
+++ livedata-ktx/src/main/java/com/shopify/livedataktx/SupportMediatorLiveData.kt
@@ -28,64 +28,24 @@ import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.Observer
 
-private typealias ObserverWrappers<T> = MutableList<Pair<Observer<T>, Observer<T>>>
-
-private fun <T> ObserverWrappers<T>.get(observer: Observer<T>): Observer<T>? =
-    indexOfFirst { it.first == observer }
-        .takeUnless { it < 0 }
-        ?.let { this[it].second }
-
-private fun <T> ObserverWrappers<T>.getOrElse(
-    observer: Observer<T>,
-    wrapper: Observer<T>
-): Observer<T> = get(observer) ?: wrapper
-
-private fun <T> ObserverWrappers<T>.putIfAbsent(observer: Observer<T>, wrapper: Observer<T>) {
-    val index = indexOfFirst { it.first == observer }
-    if (index < 0) {
-        add(Pair(observer, wrapper))
-    }
-}
-
-private fun <T> ObserverWrappers<T>.removeIfPresent(observer: Observer<T>) {
-    val index = indexOfFirst { it.first == observer }
-    if (index >= 0) {
-        removeAt(index)
-    }
-}
-
 open class SupportMediatorLiveData<T>(
     internal val isSingle: Boolean = false,
     private val versionProvider: (() -> Int)? = null
 ) : MediatorLiveData<T>() {
 
-    private val observerWrappers: ObserverWrappers<T> = mutableListOf()
+    private val observerWrappers = mutableMapOf<Observer<T>, Observer<T>>();
 
     private var _version = 0
     internal val version: Int get() = versionProvider?.let { it() } ?: _version
 
     @Deprecated("Use observe extension")
     override fun observe(owner: LifecycleOwner, observer: Observer<T>) {
-        val observerVersion = version
-        val wrapper = observerWrappers.getOrElse(observer, Observer {
-            if (!isSingle || observerVersion < version) {
-                observer.onChanged(it)
-            }
-        })
-        observerWrappers.putIfAbsent(observer, wrapper)
-        super.observe(owner, wrapper)
+        super.observe(owner, getWrapperObserver(observer))
     }
 
     @Deprecated("Use observe extension without LifecycleOwner")
     override fun observeForever(observer: Observer<T>) {
-        val observerVersion = version
-        val wrapper = observerWrappers.getOrElse(observer, Observer {
-            if (!isSingle || observerVersion < version) {
-                observer.onChanged(it)
-            }
-        })
-        observerWrappers.putIfAbsent(observer, wrapper)
-        super.observeForever(wrapper)
+        super.observeForever(getWrapperObserver(observer))
     }
 
     override fun setValue(value: T?) {
@@ -94,7 +54,20 @@ open class SupportMediatorLiveData<T>(
     }
 
     override fun removeObserver(observer: Observer<T>) {
-        observerWrappers.get(observer)?.let { super.removeObserver(it) }
-        observerWrappers.removeIfPresent(observer)
+        observerWrappers[observer]?.also {
+            super.removeObserver(it)
+            observerWrappers.remove(observer)
+        }
+    }
+
+    private fun getWrapperObserver(observer: Observer<T>): Observer<T> {
+        val observerVersion = version
+        val wrapper = observerWrappers[observer] ?: Observer {
+            if (!isSingle || observerVersion < version) {
+                observer.onChanged(it)
+            }
+        }
+        observerWrappers[observer] = wrapper
+        return wrapper
     }
 }
